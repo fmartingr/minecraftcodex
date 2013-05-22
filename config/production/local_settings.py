4152b00958ce2262054714829bc75698b1c08160
Felipe Martín <fmartin@bytepix.com>
2013-05-22 17:11:02 +0200

    Testing production local settings

--- config/production/local_settings.py
+++ config/production/local_settings.py
@@ -2,3 +2,11 @@ LOCAL_SETTINGS = True
 from settings import *
 
 DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'dev_ddbb.sqlite',
+    }
+}

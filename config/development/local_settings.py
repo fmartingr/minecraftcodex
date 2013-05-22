212ea792b354a2f1c2cb4846db2a0e565e879c96
Felipe Martín <fmartin@bytepix.com>
2013-05-22 17:59:55 +0200

    Added psycopg2 to production requirements and updated local_settings for production environment

--- config/development/local_settings.py
+++ config/development/local_settings.py
@@ -1,12 +1,31 @@
 LOCAL_SETTINGS = True
 from settings import *
+from os import environ
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'dev_ddbb.sqlite',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': environ['DATABASE_NAME'],
+        'USER': environ['DATABASE_USER'],
+        'PASSWORD': environ['DATABASE_PASS'],
+        'HOST': environ['DATABASE_HOST'],
+        'PORT': environ['DATABASE_PORT'],
     }
 }
+
+ALLOWED_HOSTS = [
+    'minecraftcodex.com',
+    'www.minecraftcodex.com',
+    'localhost',
+]
+
+# Staticfiles
+STATIC_ROOT = 'static'
+STATIC_URL = '/static/'
+
+# Mediafiles
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'

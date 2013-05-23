df77cac47871951916935be929732fb42548276b
Felipe Martín <fmartin@bytepix.com>
2013-05-23 13:02:19 +0200

    Deploy fixes: supervisor script execution, local settings for logging with raven and logging disabled on main settings

--- minecraftcodex/herobrine/settings.py
+++ minecraftcodex/herobrine/settings.py
@@ -143,20 +143,6 @@ JINJA_CONFIG = {
 }
 #from herobrine import filters
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    }
-}
 
 try:
     LOCAL_SETTINGS

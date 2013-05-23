6016bb571c56a73240b96f1b09561a4cd3062270
Felipe Martín <fmartin@bytepix.com>
2013-05-23 12:12:24 +0200

    Added supervisor install command to install script

--- config/production/install.sh
+++ config/production/install.sh
@@ -4,11 +4,9 @@ echo "=> Updating system"
 pacman -Syu --noconfirm
 
 echo "=> Installing python"
-# Install python
 pacman -S python2 python2-pip python2-virtualenv --noconfirm
 
 echo "=> Installing Postgresql"
-# Install postgresql
 pacman -S postgresql --noconfirm
 systemd-tmpfiles --create postgresql.conf
 mkdir /var/lib/postgres/data
@@ -18,24 +16,23 @@ systemctl start postgresql
 systemctl enable postgresql
 
 echo "=> Installing nginx"
-# Install nginx
 pacman -S nginx --noconfirm
 systemctl start nginx
 systemctl enable nginx
 
 echo "=> Installing git"
-# Install git
 pacman -S git --noconfirm
 
+echo "=> Installing supervisor"
+pacman -S supervisor --noconfirm
+
 echo "=> Installing nodejs and dependencies"
-# Install node & dependencies
 pacman -S nodejs --noconfirm
 npm install -g coffee-script
 npm install -g uglify-js
 npm install -g less
 
 echo "=> Creating user APP with its requirements"
-# Create site user
 useradd app
 mkdir /home/app
 chown app:app /home/app

1c94d6883e50e77397acb6a90259a1db51c94018
Felipe Martín <fmartin@bytepix.com>
2013-05-27 16:29:04 +0200

    Added ALLOWED_HOSTS variable to development settings if you wan't to text without debug = true

--- config/development/local_settings.py
+++ config/development/local_settings.py
@@ -4,6 +4,11 @@ from settings import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
